1800ce1deeb9d9060451177bb62d98dff199f2b0
Esteban9906 <quirosesteban401@gmail.com>
2026-04-15 21:05:50 -0600

    Creacion del endpoint del catalogo y modificacion del rutas de electricidad y herramientas

--- models/app.js
+++ models/app.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const { testDbConnection } = require('../services/db.js');
 
 // Middlewares
 app.use(express.json());
 
+
 // Rutas
 app.get('/', (req, res) => {
   res.json({ message: 'API funcionando correctamente' });
 });
 
+app.use(express.static(path.join(__dirname, '../public')));
+
 // Importar rutas
 const electricidadRoutes = require('../routes/electricidad.routes.js');
 const herramientasRoutes = require('../routes/herramientas.routes.js');
+const catalogoRoutes = require('../routes/catalogo.routes.js'); 
 
 app.use('/electricidad', electricidadRoutes);
 app.use('/herramientas', herramientasRoutes);
+app.use('/catalogo', catalogoRoutes);
 
 testDbConnection();
 
--- models/herramientas.model.js
+++ models/herramientas.model.js
@@ -12,6 +12,7 @@ class Herramientas {
                     sub ->> 'name' AS subcategory_name,
                     prod ->> 'id' AS product_id,
                     prod ->> 'name' AS product_name,
+                    prod ->> 'pdfPage' AS pdf_page,
                     prod -> 'images' ->> 0 AS product_image
                 FROM digital_catalog dc
                 CROSS JOIN LATERAL jsonb_array_elements(dc.content::jsonb -> 'subcategories') AS sub
@@ -35,6 +36,7 @@ class Herramientas {
                     sub ->> 'name' AS subcategory_name,
                     prod ->> 'id' AS product_id,
                     prod ->> 'name' AS product_name,
+                    prod ->> 'pdfPage' AS pdf_page,
                     COALESCE(prod -> 'features', '[]'::jsonb) AS features,
                     COALESCE(prod -> 'applications', '[]'::jsonb) AS applications,
                     COALESCE(prod -> 'images', '[]'::jsonb) AS images
@@ -54,6 +56,7 @@ class Herramientas {
                 category_name:    row.category_name,
                 subcategory_code: row.subcategory_code,
                 subcategory_name: row.subcategory_name,
+                pdf_page: row.pdf_page,
                 features:         row.features ?? [],
                 applications:     row.applications ?? [],
                 images:           row.images ?? [],
