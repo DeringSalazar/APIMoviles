a9007b8d32675ae387230d6d967c78e0e3b40a1e
DeringSS <desalazarsa@est.utn.ac.cr>
2026-04-17 17:23:40 -0600

    Add vercel config

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('./models/app.js');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
